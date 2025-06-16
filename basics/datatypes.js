8710f9f43f8fc34f0faf6c66a39ffb6003498117
harsh1507sharma <hs0797227@gmail.com>
2025-06-16 15:18:38 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict;"//treat all js codes as a newer version
+
+// alert(3+3) we are using node js not browser
+
+console.log(3+
+    
+    3)// it is correct but code readability should be high
+
+let name ="harsh"
+let age =3
+let isLoggedIn = true
+
+/*datattypes
+
+number → 2 to  the power 54
+bigint
+string→ ""
+boolean→ true/false
+null → standalone value
+undefined→not defined
+symbol → unique
+
+object (imp)
+*/
+console.log(typeof true)
+console.log(typeof undefined);//undefined khud ek tpe h 
+console.log(typeof null)
---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId =29740
+let accountemail ="harsh@apple.com"
+var acoountpass ="123456"
+accountcity ="AMROHA"
+let accountstate;
+
+// accountId =8451 not allowwwed once contant is defined 
+// prefere not to use var due to issue in functional scope and block scope
+accountemail="caikbaj@gmail.com"
+accountpass ="45487"
+accoountcity ="jalgaon"
+console.log(accountId)
+console.table([accountId,accountemail,accoountcity,accountpass,accountstate])
+
+
+
