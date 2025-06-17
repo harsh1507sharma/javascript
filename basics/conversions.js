743b52187f8145ff18a5ae5d2e8d7f492d07a86a
harsh1507sharma <hs0797227@gmail.com>
2025-06-17 08:31:27 +0000

    conversion of datatypes

---
+++ basics/conversions.js
@@ -0,0 +1,29 @@
+let score=33
+let bhaalu =true
+let aalu = "34abc"
+let value2 = Number(bhaalu)
+
+let paalu = Number(aalu)
+console.log(typeof paalu)
+console.log(paalu)
+console.log(typeof bhaalu)
+
+console.log(typeof score)
+
+let  value = String(score)
+console.log(typeof value)
+console.log(value)
+console.log(value2)
+
+// "33" → 33
+//"33abc" → NaN
+// true → 1, false →0;
+
+let isloggedin = 1
+let booleanvalue = Boolean(isloggedin)
+console.log(booleanvalue)
+
+// 1 → true;
+// 0 → false;
+// "" → false
+//"anything " → true
